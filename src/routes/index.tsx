15f884365722b1e0af2867f45d1aad629b9640c1
Felix Arhel <2018012097@dhvsu.edu.ph>
2026-02-08 01:26:39 +0800

    feat: [WIP] site styles, typography

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,15 @@ export const Route = createFileRoute("/")({
 function Index() {
   return (
     <div>
-      <h3>Welcome Home!</h3>
+      <h1>Welcome Home!</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, rerum
+        dolorum adipisci odit cum soluta optio illum vel eos impedit eligendi
+        ratione amet sequi illo maiores iure qui hic tempora quia accusantium
+        molestias nihil delectus! Vero laudantium consequatur ducimus voluptates
+        distinctio. Voluptas facere aliquam molestiae similique magnam facilis
+        expedita fuga.
+      </p>
     </div>
   );
 }
